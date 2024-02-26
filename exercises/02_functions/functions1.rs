47f2f50f242bc04d7901ec558aad0b68548cac2a
ken <ken.tsai@ckmates.com>
2024-02-26 11:17:54 +0800

    Finish exercises/02_functions

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -3,8 +3,11 @@
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
     call_me();
+}
+
+// 定義 function 從 fn 開始
+fn call_me() {
+    println!("Hi");
 }
